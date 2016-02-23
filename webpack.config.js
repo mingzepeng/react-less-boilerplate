6d85929b8fcfef37834686679ce37f0704d4c609
mingzepeng <mingzepeng@gmail.com>
2016-02-23 10:28:26 +0800

    support for deploy on ie8

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,9 @@ module.exports = {
     },
 	plugins : [ 
 		new webpack.optimize.UglifyJsPlugin({
+			compressor: {
+                warnings: false
+            },
 			mangle: {
 				except: ['$super', '$', 'exports', 'require']
 			}
